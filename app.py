b4c02d2928899684f2c66f56bc52d472f28d80d1
DhruvRadadiya007 <dhruvradadiya101@gmail.com>
2025-09-09 17:24:40 +0530

    fixed imoji error

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 app = Flask(__name__)
 CORS(app)
